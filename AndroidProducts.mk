e00316dc952dc17142386108b75b61c5b4e2876c
Khalvat-M <mmba313@gmail.com>
2020-01-24 15:30:33 +0330

    Switch from add_lunch_combo to COMMON_LUNCH_CHOICES

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,3 +15,11 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_klteduos.mk
+
+
+
+
+COMMON_LUNCH_CHOICES := \
+    lineage_klteduos-user \
+    lineage_klteduos-userdebug \
+    lineage_klteduos-eng
