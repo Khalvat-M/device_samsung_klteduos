2a11257839150fd8fd55781cfd1c5fe52b91031c
Khalvat-M <mmba313@gmail.com>
2022-02-14 14:58:46 +0330

    cleanup radio props

--- system_prop.mk
+++ system_prop.mk
@@ -1,11 +1,12 @@
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
-    DEVICE_PROVISIONED=1 \
-    ro.qualcomm.perf.cores_online=2 \
-    ro.telephony.default_network=9,9 \
     persist.multisim.config=dsds \
     persist.radio.multisim.config=dsds \
+    persist.radio.aosp_usr_pref_sel=true \
+    persist.vendor.radio.add_power_save=1 \
+    persist.vendor.radio.aosp_usr_pref_sel=true \
     ro.multisim.set_audio_params=true \
     ro.multisim.simslotcount=2 \
-    ro.multisim.set_audio_params=true \
-    ro.telephony.ril.config=simactivation
+    ro.qualcomm.perf.cores_online=2 \
+    ro.telephony.ril.config=simactivation \
+    ro.telephony.sim_slots.count=2
